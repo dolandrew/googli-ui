89df805719c3b927756f260fede1eca762e1166e
Andrew Dolan <andrew.dolan@skyward.io>
2020-03-28 12:11:12 -0700

    Build for production

---
+++ build/precache-manifest.dbc1980204872ee4d432980d5e4132db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edc4606d46addd3c52effcdfae0afb41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00a2d8712f0c0b5d555f",
+    "url": "/static/css/main.0860633e.chunk.css"
+  },
+  {
+    "revision": "0d8b68fea1b9ff5116bd",
+    "url": "/static/js/2.91c0f8d8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.91c0f8d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00a2d8712f0c0b5d555f",
+    "url": "/static/js/main.b9b8d15b.chunk.js"
+  },
+  {
+    "revision": "487f50cd5bdf9937ee64",
+    "url": "/static/js/runtime-main.b6cc3fcb.js"
+  },
+  {
+    "revision": "dace2d4f7d9941c9bc4230ca108ff1f9",
+    "url": "/static/media/phish-logo.dace2d4f.png"
+  }
+]);
