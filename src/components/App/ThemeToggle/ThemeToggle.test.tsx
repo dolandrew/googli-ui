02a5949f35a3105e8a781aa9a2189eae8290f647
adamrturman <adamrturman@gmail.com>
2021-11-18 18:54:06 -0600

    remove used

--- src/components/App/ThemeToggle/ThemeToggle.test.tsx
+++ src/components/App/ThemeToggle/ThemeToggle.test.tsx
@@ -1,7 +1,5 @@
 import ThemeToggle from "./ThemeToggle";
-import { render, fireEvent, waitForElementToBeRemoved, waitFor } from "@testing-library/react";
-import userEvent from '@testing-library/user-event';
-import { wait } from "@testing-library/user-event/dist/utils";
+import { render, fireEvent } from "@testing-library/react";
 
 const setTheme = jest.fn()
 
@@ -23,14 +21,3 @@ test('clicking the light theme toggle button fires an event', () => {
   expect(setTheme.mock.calls.length).toBe(1);
 });
 
-
-test('clicking the light theme toggle button switches the button', async () => {
-  const { getByTestId, queryByTestId } = render(<ThemeToggle theme={'#1E1E1E'} setTheme={setTheme} />);
-  const lightThemeToggle = getByTestId('light-theme-toggle');
-  const darkThemeToggle = queryByTestId('dark-theme-toggle');
-
-
-  userEvent.click(lightThemeToggle);
-
-  //  todo - assert that the correct button appears
-});
