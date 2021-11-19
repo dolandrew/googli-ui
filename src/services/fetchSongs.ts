44849a00020ad3a52fef05741179882321689922
adamrturman <adamrturman@gmail.com>
2021-11-18 18:54:06 -0600

    remove used

---
+++ src/services/fetchSongs.ts
@@ -0,0 +1,35 @@
+import GoogliResponse from "../interfaces/GoogliResponse";
+import Song from "../interfaces/Song";
+//
+// const getSongs = (setSongs: (songs: Song[]) => void, uuid: string, filter: string) => {
+//   fetch("https://googli-apparatus-backend.herokuapp.com/api/search/lyrics?uuid=" + uuid + "&filter=" + filter)
+//     .then(
+//       (response) => {
+//         response.json().then((result: GoogliResponse) => {
+//           setSongs(result.songs);
+//         })
+//       },
+//       (error) => {
+//         console.log(error);
+//       }
+//     );
+// }
+//
+// export default getSongs;
+
+
+export default (setSongs: (songs: Song[]) => void, uuid: string, filter: string) => {
+  fetch("https://googli-apparatus-backend.herokuapp.com/api/search/lyrics?uuid=" + uuid + "&filter=" + filter)
+    .then(
+      (response) => {
+        response.json().then((result: GoogliResponse) => {
+          setSongs(result.songs);
+        })
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+}
+
+
